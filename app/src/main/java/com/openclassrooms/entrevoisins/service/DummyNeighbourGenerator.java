cfd07a81db1408cf31051af9cf96f4563e18aefd
w00tw00t <w00tw00t@imt-atlantique.fr>
2023-03-31 19:07:42 +0200

    fix scrollable profile, and append more text to generated profile Caroline description

--- app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourGenerator.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourGenerator.java
@@ -16,7 +16,7 @@ public abstract class DummyNeighbourGenerator {
      */
     public static List<Neighbour> DUMMY_NEIGHBOURS = Arrays.asList(
             new Neighbour(1, "Caroline", "https://i.pravatar.cc/150?u=a042581f4e29026704d", "Saint-Pierre-du-Mont ; 5km",
-                    "+33 6 86 57 90 14",  "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner !J'aime les jeux de cartes tels la belote et le tarot.."),
+                    "+33 6 86 57 90 14",  "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner !J'aime les jeux de cartes tels la belote et le tarot.. Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla..  Bla bla bla.. "),
             new Neighbour(2, "Jack", "https://i.pravatar.cc/150?u=a042581f4e29026704e", "Saint-Pierre-du-Mont ; 5km",
                     "+33 6 86 57 90 14",  "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner !J'aime les jeux de cartes tels la belote et le tarot.."),
             new Neighbour(3, "Chloé", "https://i.pravatar.cc/150?u=a042581f4e29026704f", "Saint-Pierre-du-Mont ; 5km",
